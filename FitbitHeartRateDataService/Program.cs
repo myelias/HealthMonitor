c6524f3e2e0d2f270d26619e95d9cadf683f55d5
myelias <Martin.Yusuf.Elias1998@gmail.com>
2023-11-04 18:27:09 -0500

    Add Contract solution, RabbitMQ, and Contract ref

--- FitbitHeartRateDataService/Program.cs
+++ FitbitHeartRateDataService/Program.cs
@@ -1,4 +1,5 @@
 using FitbitHeartRateDataService.Data;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,17 @@ builder.Services.AddDbContext<HeartRateDbContext>(opt =>
     opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddMassTransit( x => {
+
+    // A Transport
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
--- SearchService/Program.cs
+++ SearchService/Program.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using MassTransit;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using Polly;
@@ -12,6 +13,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<HeartRateServiceHttpClient>().AddPolicyHandler(GetPolicy());
+builder.Services.AddMassTransit( x => {
+
+    // A Transport
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
