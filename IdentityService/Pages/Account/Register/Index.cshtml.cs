90774d6d07ccd5ea249496c1d36ff7f6425be341
Martin <Martin@MartinElias>
2023-11-24 13:25:57 -0600

    Complete Register Functionality

--- IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
+using IdentityService.Models;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using IdentityModel;
 
 
 namespace IdentityService.Pages.Register
@@ -11,9 +15,56 @@ namespace IdentityService.Pages.Register
     // unregistered to select the hyperlink
     public class Index : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        public Index(UserManager<ApplicationUser> userManager)
         {
-            
+            _userManager = userManager;
+        }
+
+        [BindProperty]
+
+        public RegisterViewModel Input {get; set;}
+
+        [BindProperty]
+
+        public bool RegisterSuccess {get; set;}
+        public IActionResult OnGet(string returnUrl) // We are returning the page so we use IActionResult
+        {
+            Input = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl,
+            };
+
+            return Page(); // Return page to user
+        }
+
+        public async Task<IActionResult> OnPost() // Async Task that will return an IActionResult
+        {
+            if (Input.Button != "register") return Redirect("~/"); // Redirect back to home page
+
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser // Create new applciation user if form has all fields populated (Valid)
+                {
+                    UserName = Input.Username,
+                    Email = Input.Email,
+                    EmailConfirmed = true
+                };
+
+                var result = await _userManager.CreateAsync(user, Input.Password );
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddClaimsAsync(user, new Claim[]
+                    {
+                        new Claim(JwtClaimTypes.Name, Input.FullName)
+                    });
+
+                    RegisterSuccess = true;
+                }
+            }
+
+            return Page();
         }
     }
 }
